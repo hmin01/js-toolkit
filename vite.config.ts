84944e408c9deb56af7b8cef8047565a81492f57
Min <goals1003@gmail.com>
2025-11-07 16:13:41 +0900

    config: 프로젝트 설정

---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import { glob } from "glob";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [],
+      input: Object.fromEntries(
+        glob
+          .sync("src/**/*.ts")
+          .filter((path) => !path.endsWith(".spec.ts"))
+          .map((path) => [
+            path
+              .replace(/^src\//, "")
+              .replace(/\.ts$/, "")
+              .replace(/\/index$/, ""),
+            resolve(__dirname, path),
+          ])
+      ),
+      output: [
+        {
+          format: "esm",
+          entryFileNames: "[name].mjs",
+          chunkFileNames: "chunks/[name]-[hash].mjs",
+          sourcemap: true,
+          hoistTransitiveImports: false,
+        },
+        {
+          format: "cjs",
+          entryFileNames: "[name].cjs",
+          chunkFileNames: "chunks/[name]-[hash].cjs",
+          sourcemap: true,
+          hoistTransitiveImports: false,
+        },
+      ],
+    },
+  },
+  plugins: [
+    dts({
+      entryRoot: "src",
+      rollupTypes: true,
+    }),
+  ],
+});
